121aa0162468cd570d84b3f5cf2fa8960485353c
Anurag Raj <adityanv8@gmail.com>
2025-09-09 11:01:23 +0530

    added_prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import dbConnect from './db/dbConnect.js'
-const app = express()
+import { PrismaClient } from '@prisma/client'
 
+const app = express()
+export const prisma = new PrismaClient()
 app.use(cookieParser())
 const PORT = process.env.PORT || 3000
 
