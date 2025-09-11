aab3259c31854b11e2b5a9f55ca731b811de3a6c
Anurag Raj <adityanv8@gmail.com>
2025-09-11 09:58:03 +0530

    user_Schema created

---
+++ backend/src/models/user.Scheam.ts
@@ -0,0 +1,38 @@
+import mongoose,{Document, Schema} from 'mongoose'
+
+export interface IUser extends Document{
+  user_name:string,
+  password:string,
+  user_image:string,
+  email:string,
+  phone_number:bigint
+}
+
+const UserSchema:Schema = new Schema({
+  user_name:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true,
+    select:false
+  },
+  user_image:{
+    type:String,
+    required:true,
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+  },
+  phone:{
+    type:BigInt,
+    required:true,
+    unique:true
+  }
+})
+
+export default mongoose.model<IUser>('users',UserSchema)
